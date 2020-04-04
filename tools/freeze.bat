057690265e8b24e79443dc8b499d8c53f0bdbdd2
Michel Zou <xantares09@hotmail.com>
2020-04-04 10:59:29 +0200

    Freeze app

---
+++ tools/freeze.bat
@@ -0,0 +1,1 @@
+cxfreeze brainworkshop.py --target-dir dist --include-files=res --include-modules=pyglet.resource,pyglet.clock,pyglet.graphics,pyglet.sprite
---
+++ tools/freeze.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cxfreeze brainworkshop.py --target-dir dist --include-files=res --include-modules=pyglet.resource,pyglet.clock,pyglet.graphics,pyglet.sprite
+
