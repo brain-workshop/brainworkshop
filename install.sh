6334b7ec7d2052fe94b9564c1d78331cd18ce886
Prajjwal Devkota <prajjwald@yahoo.com>
2021-12-25 21:09:10 -0500

    add installer for convenience - use python3

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASEDIR=$(dirname `realpath $0`)
+
+echo "Installing Dependencies";
+sudo apt install -y python3 python3-pyglet python3-future mpg123;
+
+#Let's use python3 for the program
+cd ${BASEDIR};
+sed -i 's[^#!/usr/bin/env python$[#!/usr/bin/env python3[g' brainworkshop.py;
+
+echo "Copying files to /opt and setting up desktop file";
+sudo mkdir -p /opt/;
+sudo cp -r ${BASEDIR} /opt/;
+sudo ln -s /opt/brainworkshop/brainworkshop.py /usr/local/bin/brainworkshop > /dev/null 2>&1;
+sudo cp /opt/brainworkshop/icons/brainworkshop.png /usr/local/share/pixmaps;
+sudo cp /opt/brainworkshop/brainworkshop.desktop /usr/local/share/applications;
