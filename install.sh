a8fbfb5bc62bccdaedfaffa0bf4001fb9487eb34
Prajjwal Devkota <prajjwald@yahoo.com>
2021-12-25 21:19:44 -0500

    create local dirs if not already present

--- install.sh
+++ install.sh
@@ -14,5 +14,6 @@ echo "Copying files to /opt and setting up desktop file";
 sudo mkdir -p /opt/;
 sudo cp -r ${BASEDIR} /opt/;
 sudo ln -s /opt/brainworkshop/brainworkshop.py /usr/local/bin/brainworkshop > /dev/null 2>&1;
+sudo mkdir -p /usr/local/share/pixmaps /usr/local/share/applications;
 sudo cp /opt/brainworkshop/icons/brainworkshop.png /usr/local/share/pixmaps;
 sudo cp /opt/brainworkshop/brainworkshop.desktop /usr/local/share/applications;
